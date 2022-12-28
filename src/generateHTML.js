792d15588b074c896f55577c20df0dd349e05361
Liquid Monks <info@liquidmonks.com>
2022-12-28 10:06:41 -0600

    generateHTML.js includes a correction to the console log for it to return data from index.js.

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,7 +1,7 @@
 // This file is used to generate the HTML file that will be used to display the data
 
 const generateHTML = (data) => {
-  console.log(first);
+  console.log(data);
 };
 
 // Export the generateHTML function
