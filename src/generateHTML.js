d66b0e38a33c93a273dd8bbadb090bd630e5fd99
Liquid Monks <info@liquidmonks.com>
2022-12-28 09:53:21 -0600

    src directory path includes generateHTML.js file with a function block that will generate an html file that displays data from the index.js file.

---
+++ src/generateHTML.js
@@ -0,0 +1,8 @@
+// This file is used to generate the HTML file that will be used to display the data
+
+const generateHTML = (data) => {
+  console.log(first);
+};
+
+// Export the generateHTML function
+module.exports = { generateHTML };
