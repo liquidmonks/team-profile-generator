0e823e931266b6aeddb8522136b87f39383328d4
Liquid Monks <info@liquidmonks.com>
2022-12-28 12:07:09 -0600

    generateHTML.js includes a conditional statement outline that will handle when to generate an HTML card upon user choice selection.

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,7 +1,14 @@
 // This file is used to generate the HTML file that will be used to display the data
 
 const generateHTML = (data) => {
-  console.log(data);
+  //console.log(data);
+
+  // If the user is a manager, then the manager HTML will be generated
+  if (data.manager == "y") {
+  }
+  // If the user is not a manager, then do nothing
+  else {
+  }
 };
 
 // Export the generateHTML function
