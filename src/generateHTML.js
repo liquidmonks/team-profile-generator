0de781167763c730c7eba7d807c79308646a33e9
Liquid Monks <info@liquidmonks.com>
2022-12-30 13:52:56 -0600

    generateHTML includes an additional a-tag for newManager that opens a default mail program and creates a new message with a TO field already filled out.

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -77,7 +77,7 @@ const generateHTML = (data) => {
                 <i class="fas fa-coffee"></i>
                 ${newManager.getRole()}
             </h2>
-            <p class="w-full p-4 bg-white">Email:${newManager.email}</p>
+            <p class="w-full p-4 bg-white">Email:<a href="mailto:${newManager.email}">${newManager.email}</a></p>
             <p class="w-full p-4 bg-white">Office Number:${newManager.officeNumber}</p>
         </div>
     </div>
